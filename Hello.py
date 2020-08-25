000cb9ee076e51412242bda3044cd21a3f364994
spaceblue9 <60956909+spaceblue9@users.noreply.github.com>
2020-08-25 20:09:42 +0700

    commit

--- Hello.py
+++ Hello.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-    return "Hello World! sarawut sittharod"
+    return "Hello World! eak"
 if __name__ == "__main__":
     app.run()
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:Hello --log-file=-p
+web: gunicorn Hello:Hello --log-file=-p
